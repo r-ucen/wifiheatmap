57ae3f0348fbcff948907769e80d5d691176e59b
rucen <ucen.rich@gmail.com>
2025-11-24 17:44:41 +0100

    Move the rendering logic to a dedicated class

---
+++ src/app/utils/heatmap-renderer.spec.ts
@@ -0,0 +1,7 @@
+import { HeatmapRenderer } from './heatmap-renderer';
+
+describe('HeatmapRenderer', () => {
+  it('should create an instance', () => {
+    expect(new HeatmapRenderer()).toBeTruthy();
+  });
+});
