3cf3d0961b152a941c5d062dcd42544d24fb9190
rucen <ucen.rich@gmail.com>
2025-12-08 00:47:22 +0100

    Set app icon, update favicon + title

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'final-project',
+  appName: 'WifiHeatmap',
   webDir: 'www',
 };
 
